3a4b681cf00a97cf53ad46ce8809ee797d7fd25c
aleks-git-repo <48073772+aleks-git-repo@users.noreply.github.com>
2019-03-04 10:23:05 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+//Hello everyone, my name is Aleks
+//Homework Assignment #1: Variables and Constants
+let artist :(String) = "The Beatles"
+let song = "Yellow Submarine"
+let genre = "Psychedelic rock"
+let release = "5 August, 1966"
+let length:(Float) = 2.38
+let writer = "Lennon–McCartney"
+let producer="George Martin"
+print ("          ---favorite song---","\n", "'",song,"'", artist,":" ,length,"\n",genre," produced by ",producer,"\n","---",release,writer,"---")
+/*
+ thank
+ you
+ for
+ your
+ attenntion
+*/
+//------------------------
+
