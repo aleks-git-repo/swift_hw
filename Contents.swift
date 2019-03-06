e0c5824821ae34d244642c6f0e2f53f2a03d2049
aleks-git-repo <48073772+aleks-git-repo@users.noreply.github.com>
2019-03-06 18:50:09 +0300

    Add files via upload

--- Contents.swift
+++ Contents.swift
@@ -1,18 +1,20 @@
 import UIKit
-
-//------------------------
-//Homework Assignment #2: Collections
-/*A range is an interval of values. A ranges might be more convenient for loops or other iterations.*/
-let range = 1...10
-for i in range{
-    print(i%2)
-}
+//Hello everyone, my name is Aleks
+//Homework Assignment #1: Variables and Constants
+let artist :(String) = "The Beatles"
+let song = "Yellow Submarine"
+let genre = "Psychedelic rock"
+let release = "5 August, 1966"
+let length:(Float) = 2.38
+let writer = "Lennon–McCartney"
+let producer="George Martin"
+print ("          ---favorite song---","\n", "'",song,"'", artist,":" ,length,"\n",genre," produced by ",producer,"\n","---",release,writer,"---")
 /*
- A tuple is a grouping of ordered values which does not need to be the same type(int,float,string,bool etc. may be in one collection).
- An array is mutable collection with one type of elements. Place in memory for array should be allocate in advance. As a consequence array needs to have a fixed size.
- */
-var tuple=(num: 5, str: "string", char: "c", bool: "true", float: "3.141295")
-var pets_arr:[String]=["cat","dog","fish","guinea pig","bird"]
-for i in pets_arr{
-    print(i)
-}
+ thank
+ you
+ for
+ your
+ attenntion
+*/
+//------------------------
+
